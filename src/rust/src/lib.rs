1f35264b10c70f7ed2be0fcca9293fa3742d395c
Robin Lovelace <rob00x@gmail.com>
2021-12-29 09:47:55 +0000

    Try with for loop

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -5,6 +5,7 @@ use extendr_api::prelude::*;
 use geo::{winding_order::Points, Coordinate, Point};
 use geojson::{FeatureCollection, Feature, Geometry, Value};
 use serde_json::to_string_pretty;
+use serde::Deserialize;
 
 /// Return string `"Hello world!"` to R.
 /// @export
@@ -16,36 +17,57 @@ fn hello_world() -> &'static str {
 /// @export
 #[extendr]
 pub fn csv_to_geojson_rust() {
-    let mut reader = csv::Reader::from_path("points.csv").unwrap();
+    let geojson_path = "points_rust.geojson";
+    let csv_path = "points.csv";
+    let mut reader = csv::Reader::from_path(csv_path).unwrap();
 
     #[derive(Deserialize)]
-    struct rustpoint {
+    struct Rustpoint {
       x: f64,
       y: f64,
     }
 
-    let points = reader
-        .records()
-        // this will silently discard invalid / unparseable records
-        .filter_map(|record| record.ok())
-        .map(|record| {
-            Feature::from(Value::from(Value::Point(rustpoint {
-                x: record[0].parse().unwrap(),
-                y: record[1].parse().unwrap(),
-            })))
-        })
-        .collect();
-    let fc: FeatureCollection<> = FeatureCollection {
-        bbox: None,
-        features: points,
-        foreign_members: None,
-    };
-    let geojson_string = to_string_pretty(&fc).unwrap();
-    serde_json::to_writer_pretty(
-        &mut File::create("points_rust.geojson").unwrap(),
-        &fc,
-    )
-    .unwrap();
+
+
+    for rec in csv::Reader::from_path(csv_path).unwrap().deserialize() {
+        let rustpoint: Rustpoint = rec.unwrap();
+        print!("{}", rustpoint.x);
+        // let point = Point::new(rustpoint.x, rustpoint.y);
+        // let feature = Feature::new(
+        //     Some(Geometry::new(Value::Point(point))),
+        //     None,
+        //     None,
+        // );
+        // let feature_collection = FeatureCollection::new(vec![feature]);
+        // let geojson = to_string_pretty(&feature_collection).unwrap();
+        // let mut file = File::create(geojson_path).unwrap();
+        // file.write_all(geojson.as_bytes()).unwrap();
+    }
+
+
+    // let points = reader
+    //     .records()
+    //     // this will silently discard invalid / unparseable records
+    //     .filter_map(|record| record.ok())
+    //     .map(|record| {
+    //         Feature::from(Value::from(Value::Point(Rustpoint {
+    //             x: record[0],
+    //             y: record[1],
+    //         })))
+    //     })
+    //     .collect();
+
+    // let fc: FeatureCollection<> = FeatureCollection {
+    //     bbox: None,
+    //     features: points,
+    //     foreign_members: None,
+    // };
+    // let geojson_string = to_string_pretty(&fc).unwrap();
+    // serde_json::to_writer_pretty(
+    //     &mut File::create(geojson_path).unwrap(),
+    //     &fc,
+    // )
+    // .unwrap();
 }
 // Macro to generate exports.
 // This ensures exported functions are registered with R.
